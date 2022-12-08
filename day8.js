bfee13545a764a476b6c123222802d4cbad74034
Lionel Manzetti <lionel.manzetti@gmail.com>
2022-12-08 13:57:01 +0100

    Should work out fine

--- day8.js
+++ day8.js
@@ -3,6 +3,7 @@ const fs = require("fs/promises");
 async function start() {
 	const file = await fs.readFile("day8_input.txt", { encoding: "utf8" });
 	let visibleTrees = 0;
+	let highScore = 0;
 
 	const totalLines = file
 		.split("\n")
@@ -17,43 +18,70 @@ async function start() {
 				} else {
 					//check left row
 					let leftView = true;
+					let leftScore = 0;
 					for (let i = treeIndex; i > 0; i--) {
 						if (trees[i - 1] >= tree) {
 							leftView = false;
+							leftScore = treeIndex - i + 1;
 						}
+					}
+					if (leftView) {
+						leftScore = treeIndex;
 					}
 
 					//check right row
 					let rightView = true;
+					let rightScore = 0;
 					for (let i = treeIndex; i < trees.length; i++) {
 						if (trees[i + 1] >= tree) {
 							rightView = false;
+							rightScore = i - treeIndex + 1;
 						}
+					}
+					if (rightView) {
+						rightScore = trees.length - treeIndex + 1;
 					}
 
 					//check top column
 					let topView = true;
+					let topScore = 0;
 					for (let i = lineIndex; i > 0; i--) {
 						if (totalLines[i - 1][treeIndex] >= tree) {
 							topView = false;
+							topScore = lineIndex - i + 1;
 						}
+					}
+					if (topView) {
+						topScore = lineIndex;
 					}
 
 					//check bottom column
 					let bottomView = true;
+					let bottomScore = 1;
 					for (let i = lineIndex; i < totalLines.length - 1; i++) {
 						if (totalLines[i + 1][treeIndex] >= tree) {
 							bottomView = false;
+							bottomScore = i - lineIndex + 1;
 						}
+					}
+					if (bottomView) {
+						bottomScore = totalLines.length - lineIndex + 1;
 					}
 
 					if (leftView || rightView || topView || bottomView) {
 						visibleTrees++;
 					}
+					const currentScore = leftScore * rightScore * topScore * bottomScore;
+
+					if (currentScore > highScore) {
+						highScore = currentScore;
+					}
 				}
 			}
 		});
 	});
+
 	console.log(visibleTrees);
+	console.log(highScore);
 }
 start();
